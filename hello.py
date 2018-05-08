ad1b19c77e889c44f8efe54ffae41a7dbbdbe308
tashamk <tashamckenzie@gmail.com>
2018-05-08 23:07:11 +0100

    meh

--- hello.py
+++ hello.py
@@ -26,7 +26,7 @@ def alineresult():
     length = float (request.form['length'])
     waist = float (request.form['waist'])
     hip = float (request.form['hip'])
-    waist_hip_distance = float(request.form['waist_hip_distance']
+    waist_hip_distance = float(request.form['waist_hip_distance'])
     return render_template("aline.html", pattern_waist = waist/2)
 
 
